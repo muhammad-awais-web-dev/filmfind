6c0627832fcf333adfe27e8de56f34da1ef4b463
Muhammad Awais <awaisrafique4929@gmail.com>
2025-09-14 05:47:41 +0500

    Initial commit

---
+++ src/API/tmbd.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = process.env.REACT_APP_API_KEY;
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.AUTHORIZATION}`
+  }
+};
+export async function fetchPopularMovies(page = 1){
+    try {
+        const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`, options);
+        const json = await res.json();
+        return json;
+    } catch (err) {
+        return console.error(err);
+    }
+}
