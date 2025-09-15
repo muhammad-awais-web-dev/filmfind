7c9297503eb526a94a994081586d8ed5e0ec783e
Muhammad Awais <awaisrafique4929@gmail.com>
2025-09-15 22:06:56 +0500

    Bijli Gai

--- src/pages/home/TV.js
+++ src/pages/home/TV.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './TV.module.css'
-import MovieCard from '../../components/MovieCard';
+import TvCard from '../../components/TvCard';
 import {tv} from '../../API/tmbd';
 import { useState } from 'react';
 
@@ -9,18 +9,6 @@ const tvOnTheAir = await tv('on_the_air');
 const tvPopular = await tv('popular');
 const tvTopRated = await tv('top_rated');
 
-tvAiringToday.results.map( (movie) => (
-    movie.title = movie.name
-));
-tvOnTheAir.results.map( (movie) => (
-    movie.title = movie.name
-));
-tvPopular.results.map( (movie) => (
-    movie.title = movie.name
-));
-tvTopRated.results.map( (movie) => (
-    movie.title = movie.name
-));
 
 function TV() {
 
@@ -56,22 +44,22 @@ function TV() {
             </div>
             <div style={{ display: selectedList === 'airing_today' ? 'flex' : 'none' }} className={styles.trendingList}>
               {tvAiringToday.results.map( (movie) => (
-                  <MovieCard key={movie.id} movie={movie} />
+                  <TvCard key={movie.id} movie={movie} />
               ))}
             </div>
             <div style={{ display: selectedList === 'on_the_air' ? 'flex' : 'none' }} className={styles.trendingList}>
               {tvOnTheAir.results.map( (movie) => (
-                  <MovieCard key={movie.id} movie={movie} />
+                  <TvCard key={movie.id} movie={movie} />
               ))}
             </div>
             <div style={{ display: selectedList === 'popular' ? 'flex' : 'none' }} className={styles.trendingList}>
               {tvPopular.results.map( (movie) => (
-                  <MovieCard key={movie.id} movie={movie} />
+                  <TvCard key={movie.id} movie={movie} />
               ))}
             </div>
             <div style={{ display: selectedList === 'top_rated' ? 'flex' : 'none' }} className={styles.trendingList}>
               {tvTopRated.results.map( (movie) => (
-                  <MovieCard key={movie.id} movie={movie} />
+                  <TvCard key={movie.id} movie={movie} />
               ))}
             </div>
     </section>
