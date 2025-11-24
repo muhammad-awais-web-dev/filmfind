900c99246987b276838e02370b1803154ca1b5a6
Muhammad Awais <awaisrafique4929@gmail.com>
2025-11-24 18:36:35 +0500

    Moving from render to vercel.

--- src/API/tmbd.js
+++ src/API/tmbd.js
@@ -1,6 +1,7 @@
 export async function API( option = '', additional = '') {
     try {
-        const res = await fetch(`https://backend-for-apis.onrender.com/api/tmdb/${option}?${additional}`);
+        const res = await fetch(`https://backend-api-handler.vercel.app/api/tmdb/${option}?${additional}`);
+        // const res = await fetch(`https://backend-for-apis.onrender.com/api/tmdb/${option}?${additional}`);
         if (!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
         }
